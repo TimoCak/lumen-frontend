8361c87eddd334703787dd8bdffddd25dea1f19f
Timo Caktu <timocaktu@gmail.com>
2023-07-20 00:32:13 +0200

    request architecture changes

--- src/js.rs
+++ src/js.rs
@@ -14,4 +14,9 @@ extern "C" {
    #[wasm_bindgen(js_namespace = location)]
    pub fn href(s: String);
 
+   #[wasm_bindgen(js_namespace = localStorage)]
+   pub fn setItem(key: String, value: JsValue);
+   pub fn getItem(key: String);
+   pub fn removeItem(key: String);
+
 }
--- src/requests/post_login.rs
+++ src/requests/post_login.rs
@@ -3,19 +3,24 @@ use serde::{Deserialize, Serialize};
 use yew::UseStateSetter;
 use yew_router::prelude::Navigator;
 
+use crate::js::setItem;
 use crate::{get_backend_url, router::Route};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
     username: String,
-    password: String
+    password: String,
 }
 
-pub fn post_login(username: &String, password: &String, navigator: Navigator, response_text: UseStateSetter<String>) {
-
+pub fn post_login(
+    username: &String,
+    password: &String,
+    navigator: Navigator,
+    response_text: UseStateSetter<String>,
+) {
     let user = User {
         username: username.to_owned(),
-        password: password.to_owned()
+        password: password.to_owned(),
     };
 
     wasm_bindgen_futures::spawn_local(async move {
@@ -27,14 +32,16 @@ pub fn post_login(username: &String, password: &String, navigator: Navigator, re
             .body(serde_json::to_string(&user).unwrap())
             .send()
             .await
-            .unwrap()
-            .text()
-            .await
             .unwrap();
 
-        response_text.set(response.clone());
+        let response_body = response.text().await.unwrap();
 
-        if response.eq("succesfully logged in!") {
+        if response.status() == 400 {
+            response_text.set("please fill out all fields!".to_owned());
+        } else if response.status() == 401 {
+            response_text.set("wrong username or password!".to_owned());
+        } else if response.status() == 200 {
+            setItem("currentUser".to_owned(), response_body.into());
             navigator.push(&Route::Home);
         }
     });
